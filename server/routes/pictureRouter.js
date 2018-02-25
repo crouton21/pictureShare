fc310bb6b514add2b8298c49772128a72c73380b
Christiana Routon <christiana.routon@gmail.com>
2018-02-24 20:38:55 -0600

    added comments

--- server/routes/pictureRouter.js
+++ server/routes/pictureRouter.js
@@ -32,6 +32,34 @@ router.get('/', function(request, response){
       })
   })
 
+  router.post('/comment/:id', function(request, response){
+      const id = request.params.id;
+      const comment = request.body.comment;
+      console.log('comment on backend:', comment);
+      const sqlText = `INSERT INTO comments (photo_id, comment) VALUES ($1, $2)`;
+      pool.query(sqlText, [id, comment])
+      .then(function(result) {
+        response.sendStatus(200);
+      })
+      .catch(function(error) {
+        console.log('Error on adding comment');
+        response.sendStatus(500);
+      })
+  })
+
+  router.get('/comments', function(request, response){
+    const sqlText = 'SELECT photo_id, comment FROM comments JOIN photos ON photos.id=comments.photo_id ORDER BY comments.time_submitted DESC';
+    pool.query(sqlText)
+      .then(function(result) {
+        console.log('Get result:', result);
+        response.send(result.rows);
+      })
+      .catch(function(error){
+        console.log('Error on GET comments:', error);
+        response.sendStatus(500);
+      })
+  })
+
 
 
 module.exports = router;
